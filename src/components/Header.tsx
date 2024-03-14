30b5e07614a774c56b1e1c2759c113f43a675f70
sockki <poohct327@gmail.com>
2024-03-14 16:10:21 +0900

    Feat AdminMenu 구현 - #12

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,31 @@
 import * as S from "../styles/Header.styles";
 import Logo from "../assets/img_logo.png";
 import { getMenuList } from "../hooks/getMenuList";
-import { useState } from "react";
-import { useNavigate } from "react-router";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router";
 
 export default function Header() {
-  const [login] = useState({ user: "user" });
+  const [login, setLogin] = useState({ user: "user" });
+  const [onMenu, setonMenu] = useState<boolean>(false);
 
   const menuList = getMenuList(login);
   const navigate = useNavigate();
+  const locationObject = useLocation();
 
   const handleMoveToMenu = (path: string) => {
     navigate(path);
   };
+
+  const handleMenu = () => {
+    setonMenu(!onMenu);
+  };
+
+  // 로그인 구현 전에 임시로 만든 코드입니다.
+  useEffect(() => {
+    if (locationObject.pathname.includes("admin")) {
+      setLogin({ user: "admin" });
+    }
+  }, []);
 
   return (
     <S.HeaderContainer>
@@ -20,16 +33,47 @@ export default function Header() {
         <img src={Logo} alt="로고" />
       </S.LogoContainer>
       <S.MenuContainer>
-        {menuList.map((menu: any, index: number) => (
+        {login.user !== "admin" ? (
+          menuList.map((menu: any, index: number) => (
+            <S.Menu
+              key={index}
+              onClick={() => {
+                handleMoveToMenu(menu.path);
+              }}
+            >
+              {menu.label}
+            </S.Menu>
+          ))
+        ) : (
           <S.Menu
-            key={index}
             onClick={() => {
-              handleMoveToMenu(menu.path);
+              handleMenu();
             }}
           >
-            {menu.label}
+            MENU
           </S.Menu>
-        ))}
+        )}
+        {onMenu && (
+          <S.Overlay
+            onClick={() => {
+              handleMenu();
+            }}
+          >
+            <S.AdminMenuContainer>
+              <S.AdminMenuTitle>MENU</S.AdminMenuTitle>
+              {menuList.map((menu: any, index: number) => (
+                <S.AdminMenuContent
+                  key={index}
+                  onClick={() => {
+                    handleMoveToMenu(menu.path);
+                  }}
+                >
+                  {menu.label}
+                </S.AdminMenuContent>
+              ))}
+            </S.AdminMenuContainer>
+          </S.Overlay>
+        )}
       </S.MenuContainer>
     </S.HeaderContainer>
   );
--- src/styles/Header.styles.ts
+++ src/styles/Header.styles.ts
@@ -31,6 +31,7 @@ export const LogoContainer = styled.div`
 `;
 
 export const MenuContainer = styled.div`
+  position: relative;
   display: flex;
   align-items: center;
   gap: 20px;
@@ -44,3 +45,47 @@ export const Menu = styled.button`
     font-size: var(--font-size-600);
   }
 `;
+
+export const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`
+
+export const AdminMenuContainer = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 300px;
+  height: 500px;
+  border-bottom-left-radius: 20px;
+  background-color: var(--main-color-300);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 30px;
+
+  @media screen and (max-width: ${breakPoints.small}px) {
+    width: 250px;
+  }
+`
+
+export const AdminMenuTitle = styled.div`
+  font-size: var(--font-size-300);
+  font-weight: 700;
+  margin-top: 30px;
+  margin-bottom: 20px;
+  @media screen and (max-width: ${breakPoints.small}px) {
+    font-size: var(--font-size-500);
+  }
+`
+
+export const AdminMenuContent = styled.div`
+  font-size: var(--font-size-400);
+
+  @media screen and (max-width: ${breakPoints.small}px) {
+    font-size: var(--font-size-600);
+  }
+`
