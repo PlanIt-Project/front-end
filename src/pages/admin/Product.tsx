c3731d38d5c39d0d41441d30c4a9a856b5ba2be8
sockki <poohct327@gmail.com>
2024-03-17 17:04:59 +0900

    Feat Product Modal 퍼블리싱 - #12

--- src/pages/admin/Product.tsx
+++ src/pages/admin/Product.tsx
@@ -1,17 +1,86 @@
+import { useRef, useState } from "react";
 import ProductBox from "../../components/admin/ProductBox";
 import * as S from "../../styles/admin/Product.styles";
+import CommonSelectBox from "../../components/CommonSelectBox";
+
+const type = [
+  { label: "1", value: "패키지" },
+  { label: "2", value: "이용권" },
+];
 
 export default function Product() {
-  
+  const [onModal, setOnModal] = useState(false);
+  const [boxOpen, setBoxOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("패키지");
+  const TypeSelectRef = useRef<HTMLDivElement>(null);
+  const onClickMakeButton = () => {
+    setOnModal(!onModal);
+  };
 
   return (
-    <S.AdminContainer>
-      <S.AdminContent>
-        <S.Title>상품관리</S.Title>
-        <S.MakeButton>상품추가</S.MakeButton>
-        <ProductBox/>
-      </S.AdminContent>
-    </S.AdminContainer>
+    <>
+      <S.AdminContainer>
+        <S.AdminContent>
+          <S.Title>상품관리</S.Title>
+          <S.MakeButton
+            onClick={() => {
+              onClickMakeButton();
+            }}
+          >
+            상품추가
+          </S.MakeButton>
+          <ProductBox />
+        </S.AdminContent>
+      </S.AdminContainer>
+      {onModal && (
+        <S.Overlay>
+          <S.Modal>
+            <S.ModalContent>
+              <S.ContentName>이름</S.ContentName>
+              <S.ContentInput width={270} placeholder="홍길동" />
+            </S.ModalContent>
+            <S.ModalContent>
+              <S.ContentName>종류</S.ContentName>
+              <CommonSelectBox
+                selectBoxRef={TypeSelectRef}
+                isSelectBoxOpen={boxOpen}
+                setIsSelectBoxOpen={setBoxOpen}
+                selectedOption={selectedOption}
+                setSelectedOption={setSelectedOption}
+                optionList={type}
+                width={270}
+                height={20}
+              />
+            </S.ModalContent>
+            <S.ModalContent>
+              <S.ContentName>기간</S.ContentName>
+              <S.InputContainer>
+                <S.ContentInput width={40} />
+                <S.InputText>년</S.InputText>
+                <S.ContentInput width={40} />
+                <S.InputText>월</S.InputText>
+                <S.ContentInput width={40} />
+                <S.InputText>일</S.InputText>
+              </S.InputContainer>
+            </S.ModalContent>
+            <S.ModalContent>
+              <S.ContentName>횟수</S.ContentName>
+              <S.InputContainer>
+                <S.ContentInput width={250} />
+                <S.InputText>회</S.InputText>
+              </S.InputContainer>
+            </S.ModalContent>
+            <S.ModalContent>
+              <S.ContentName>가격</S.ContentName>
+              <S.InputContainer>
+                <S.ContentInput width={250} />
+                <S.InputText>원</S.InputText>
+              </S.InputContainer>
+            </S.ModalContent>
+            <S.ModalButton>등록하기</S.ModalButton>
+          </S.Modal>
+        </S.Overlay>
+      )}
+    </>
   );
 }
-
--- src/styles/admin/Product.styles.ts
+++ src/styles/admin/Product.styles.ts
@@ -84,4 +84,75 @@ export const ModifyButton = styled(CommonButton)`
   &:hover {
     background-color: var(--main-color-300);
   }
-`
+`;
+
+export const Overlay = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+  z-index: 2;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.25);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Modal = styled.div`
+  width: 600px;
+  height: 500px;
+  background: var(--background-color-100);
+  border-radius: 20px;
+  padding-top: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 30px;
+`;
+
+export const ModalContent = styled.div`
+  display: flex;
+  align-items: center;
+  align-self: flex-start;
+  gap: 80px;
+  width: 100%;
+  margin-left: 100px;
+`;
+export const ContentName = styled.span`
+  font-size: var(--font-size-400);
+`;
+
+export const ContentInput = styled.input<{
+  width: number;
+}>`
+  background: none;
+  border: 1px solid var(--main-color-300);
+  border-radius: 30px;
+  height: 20px;
+  width: ${(props) => `${props.width}px`};
+  height: 25px;
+  padding: 10px;
+  padding-left: 20px;
+  font-size: var(--font-size-600);
+  ::placeholder {
+    color: var(--white-color-700);
+    font-size: var(--font-size-600);
+    font-weight: 600;
+  }
+  &:focus {
+    outline: none;
+    outline: 1px solid var(--main-color-500);
+  }
+`;
+
+export const InputText = styled.span`
+  font-size: var(--font-size-500);
+`;
+export const InputContainer = styled.div`
+  width: 300px;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+export const ModalButton = styled(CommonButton)``;
