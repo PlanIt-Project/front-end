93e04fbaf54b37fc6dd84cff53ca01095b44685d
sockki <poohct327@gmail.com>
2024-03-18 12:36:53 +0900

    Fix confilct 해결

---
+++ src/components/menu/AdminMenu.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { getMenuList } from "../../hooks/getMenuList";
+import { useNavigate } from "react-router";
+import * as S from "../../styles/menu/Menu.styles";
+import { ILogin } from "../../types/Login.types";
+
+export default function AdminMenu({ login }: { login: ILogin }) {
+  const [onMenu, setonMenu] = useState<boolean>(false);
+  const menuList = getMenuList(login);
+  const navigate = useNavigate();
+
+  const handleMoveToMenu = (path: string) => {
+    navigate(path);
+  };
+
+  const handleMenu = () => {
+    setonMenu(!onMenu);
+  };
+
+  return (
+    <>
+      <S.Content
+        onClick={() => {
+          handleMenu();
+        }}
+      >
+        MENU
+      </S.Content>
+      {onMenu && (
+        <S.Overlay
+          onClick={() => {
+            handleMenu();
+          }}
+        >
+          <S.AdminMenuContainer>
+            <S.AdminMenuTitle>MENU</S.AdminMenuTitle>
+            {menuList.map((menu: any, index: number) => (
+              <S.AdminMenuContent
+                key={index}
+                onClick={() => {
+                  handleMoveToMenu(menu.path);
+                }}
+              >
+                {menu.label}
+              </S.AdminMenuContent>
+            ))}
+          </S.AdminMenuContainer>
+        </S.Overlay>
+      )}
+    </>
+  );
+}
