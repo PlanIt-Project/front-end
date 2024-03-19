5f8cbec14e59c50d3dad42473b2d9d182523ac1c
HyerinJeon <hyerin0930@gmail.com>
2024-03-19 11:07:27 +0900

    Design TrainerSchedule page #10

--- src/pages/trainer/Schedule.tsx
+++ src/pages/trainer/Schedule.tsx
@@ -1,3 +1,45 @@
+import { useState } from "react";
+import TabMenu from "../../components/TabMenu";
+import Calendar from "../../components/schedule/Calendar";
+import * as S from "../../styles/Schedule.styles";
+import { TODAY } from "../../constants/Calendar.constants";
+import Time from "../../components/schedule/Time";
+
 export default function TrainerSchedule() {
-  return <div>트레이너 스케줄</div>;
+  const unavailableTimes = ["6:00", "14:00", "18:00"];
+
+  const [selectedDay, setSelectedDay] = useState(TODAY);
+  const [selectedTime, setSelectedTime] = useState("");
+
+  const handleClickDay = (day: Date) => {
+    setSelectedDay(day);
+  };
+
+  const isTimeAvailable = (time: string) => {
+    return !unavailableTimes.includes(time);
+  };
+
+  const handleClickTime = (time: string) => {
+    setSelectedTime(time);
+  };
+
+  return (
+    <S.Container>
+      <S.TopContainer className="trainer">
+        <TabMenu />
+      </S.TopContainer>
+      <S.CalendarContainer>
+        <Calendar selectedDay={selectedDay} handleClickDay={handleClickDay} />
+      </S.CalendarContainer>
+      <S.BottomContainer className="reservation">
+        <Time
+          selectedDay={selectedDay}
+          selectedTime={selectedTime}
+          isTimeAvailable={isTimeAvailable}
+          handleClickTime={handleClickTime}
+        />
+        <S.ReservationButton>설정하기</S.ReservationButton>
+      </S.BottomContainer>
+    </S.Container>
+  );
 }
