e8818e005011f6d1f022ac0190a1ae69b441e098
sockki <poohct327@gmail.com>
2024-03-26 23:53:26 +0900

    Fix 디자인 수정 - #12

--- src/components/admin/AccountBox.tsx
+++ src/components/admin/AccountBox.tsx
@@ -22,14 +22,17 @@ export default function AccountBox() {
         </S.NameBar>
         <S.ContentContainer>
           {ACCOUNT_CONTENTS.map((content) => (
-            <S.ContentBar key={content.id} $nameNumber={8}>
-              <S.Content key={"id"}>{content.id}</S.Content>
-              <S.Content key={"name"}>{content.name}</S.Content>
-              <S.Content key={"emailId"}>{content.emailId}</S.Content>
-              <S.Content key={"birthday"}>{content.birthday}</S.Content>
-              <S.Content key={"phoneNumber"}>{content.phoneNumber}</S.Content>
-              <S.Content key={"gender"}>{content.gender}</S.Content>
-              <S.Content key={"permission"}>{content.permission}</S.Content>
+            <S.ContentBar key={content.id} $nameNumber={7}>
+              <S.ContentHover $nameNumber={7}>
+                <S.Content key={"id"}>{content.id}</S.Content>
+                <S.Content key={"name"}>{content.name}</S.Content>
+                <S.Content key={"emailId"}>{content.emailId}</S.Content>
+                <S.Content key={"birthday"}>{content.birthday}</S.Content>
+                <S.Content key={"phoneNumber"}>{content.phoneNumber}</S.Content>
+                <S.Content key={"gender"}>{content.gender}</S.Content>
+                <S.Content key={"permission"}>{content.permission}</S.Content>
+              </S.ContentHover>
+
               <S.ModifyButton
                 onClick={() => {
                   onClickModifyButton();
