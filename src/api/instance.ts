fa8b4fbe547deb84d16aaccb7cb5861a6bdebd13
Haileyyyy <107675322+Hailey0930@users.noreply.github.com>
2024-03-27 15:11:18 +0900

    Fix refreshToken bug (#40)
    * master merge (#27)
    
    * Style --background-color-100 추가 - #12
    
    * Feat 상품 관리 페이지 퍼블리싱 - #12
    
    * Fix styles, constant 분리 - #12
    
    * [ADD] signup states - #17
    
    * Fix overflow-y 추가 - #12
    
    * Rename adminRequest typo #2
    
    * Feat Product Modal 퍼블리싱 - #12
    
    * Refactor getMenuList function #2
    
    * Refactor Menu, AdminMenu with Login type #2
    
    * [FIX] 간단하게 코드 수정 -#17
    
    * Fix 함수 띄어쓰기 - #12
    
    * Fix 함수 띄어쓰기 - #12
    
    * Fix confilct 해결
    
    * Feat 판매 여부 추가 - #12
    
    * Feat instance, react-query with API
    
    * Feat 버튼 클릭시 modal 열리도록 수정 - #12
    
    * Fix isModify 상태에 따른 text 수정
    
    * Docs TODO 기입 - #12
    
    * Fix 정렬 - #12
    
    * Feat api folder example #2
    
    * [FEAT] 로그인 api service 추가
    
    * Feat TabMenu component #10
    
    * Design TrainerReservation page #10
    
    * Fix currentTab bug in TabMenu component #10
    
    * Design TrainerSchedule page #10
    
    * [FEAT] 전에 피드백 반영 + getNewRefreshToken 생성
    
    * [FEAT] 로그인, 회원가입 API연결 정상작동 확인
    
    * Remove github action
    
    ---------
    
    Co-authored-by: sockki <poohct327@gmail.com>
    Co-authored-by: chobuzz <chobuzz23@gmail.com>
    Co-authored-by: chobuzz <139810780+chobuzz@users.noreply.github.com>
    
    * Dev (#29)
    
    * Style --background-color-100 추가 - #12
    
    * Feat 상품 관리 페이지 퍼블리싱 - #12
    
    * Fix styles, constant 분리 - #12
    
    * [ADD] signup states - #17
    
    * Fix overflow-y 추가 - #12
    
    * Rename adminRequest typo #2
    
    * Feat Product Modal 퍼블리싱 - #12
    
    * Refactor getMenuList function #2
    
    * Refactor Menu, AdminMenu with Login type #2
    
    * [FIX] 간단하게 코드 수정 -#17
    
    * Fix 함수 띄어쓰기 - #12
    
    * Fix 함수 띄어쓰기 - #12
    
    * Fix confilct 해결
    
    * Feat 판매 여부 추가 - #12
    
    * Feat instance, react-query with API
    
    * Feat 버튼 클릭시 modal 열리도록 수정 - #12
    
    * Fix isModify 상태에 따른 text 수정
    
    * Docs TODO 기입 - #12
    
    * Fix 정렬 - #12
    
    * Feat api folder example #2
    
    * [FEAT] 로그인 api service 추가
    
    * Feat TabMenu component #10
    
    * Design TrainerReservation page #10
    
    * Fix currentTab bug in TabMenu component #10
    
    * Design TrainerSchedule page #10
    
    * [FEAT] 전에 피드백 반영 + getNewRefreshToken 생성
    
    * [FEAT] 로그인, 회원가입 API연결 정상작동 확인
    
    * Remove github action
    
    * Fix netlify redirect bug #2 (#28)
    
    ---------
    
    Co-authored-by: sockki <poohct327@gmail.com>
    Co-authored-by: chobuzz <chobuzz23@gmail.com>
    Co-authored-by: chobuzz <139810780+chobuzz@users.noreply.github.com>
    
    * Edit TabMenu with ticket page #30
    
    * Style add folder for modal styles #30
    
    * Feat TicketRegisterModal #30
    
    * Rename router in ticket page
    
    * Design Ticket page #30
    
    * Fix instance use #30
    
    * Feat TicketList API #30
    
    * Feat TicketList API in Ticket page & Design Ticket page #30
    
    * Feat ProgramList API #30
    
    * Feat Login, refreshToken, getUserInfo API #34
    
    * Fix Header with login #34
    
    * Install dayjs #30
    
    * Feat ToastNotification component #30
    
    * Edit CommonSelectBox #30
    
    * Feat programList API, registerTicket API in ticket page #30
    
    * Feat LoginRoute, getMenuList with zustand
    
    * Feat error handler
    
    * Fix refresh bug (-ing)
    
    * Fix refresh bug (-ing)
    
    * Fix refresh bug #30
    
    ---------
    
    Co-authored-by: sockki <poohct327@gmail.com>
    Co-authored-by: chobuzz <chobuzz23@gmail.com>
    Co-authored-by: chobuzz <139810780+chobuzz@users.noreply.github.com>

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -39,19 +39,16 @@ instance.interceptors.response.use(
       response: { status },
     } = error;
 
-    console.log("찍힘?");
-
     const originalRequest = config;
 
     if (status === 401 && !originalRequest._retry) {
-      console.log("찍힘2?");
       try {
         // refreshToken으로 새 토큰 발급
         const { refreshToken } = useAuthStore.getState();
+        const response = await getRefreshTokenService(refreshToken);
 
-        await getRefreshTokenService(refreshToken).then(async (res) => {
-          console.log("찍힘?3");
-          const { accessToken, refreshToken: newRefreshToken } = res.data;
+        if (response) {
+          const { accessToken, refreshToken: newRefreshToken } = response.data;
 
           // 새 토큰 저장
           useAuthStore.setState({
@@ -65,7 +62,7 @@ instance.interceptors.response.use(
           originalRequest._retry = true;
 
           return await instance(originalRequest);
-        });
+        }
       } catch (error) {
         console.log(error);
         return await Promise.reject(error);
