e4204819073b3695b98b764370868991c4ffa233
HyerinJeon <hyerin0930@gmail.com>
2024-03-27 15:49:48 +0900

    Feat edit handleGetStart with zustand #9

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -4,8 +4,11 @@ import { setVH } from "../utils/setVH";
 import Kettlebell from "../assets/img_kettlebell.png";
 import Dumbell from "../assets/img_dumbell.png";
 import { useNavigate } from "react-router";
+import { useAuthStore } from "../stores/authStore";
 
 export default function Landing() {
+  const { user } = useAuthStore((state) => state);
+
   useEffect(() => {
     window.addEventListener("resize", setVH);
     setVH();
@@ -13,8 +16,8 @@ export default function Landing() {
 
   const navigate = useNavigate();
 
-  const handleMoveToLogin = () => {
-    navigate("/login");
+  const handleGetStart = () => {
+    user?.role ? navigate("/main") : navigate("/login");
   };
 
   return (
@@ -24,7 +27,7 @@ export default function Landing() {
           <p>PT의 모든 것</p>
           <p>PlanIT에서 쉽고 간편하게</p>
         </S.Title>
-        <S.StartButton onClick={handleMoveToLogin}>Get Start</S.StartButton>
+        <S.StartButton onClick={handleGetStart}>Get Start</S.StartButton>
       </S.TitleButtonContainer>
       <S.KettlebellContainer>
         <img src={Kettlebell} alt="kettlebell" />
